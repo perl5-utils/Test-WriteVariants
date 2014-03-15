f3b2741c0c5e9d88ffa6bc4aec57d9309072a24c
Tim Bunce <Tim.Bunce@pobox.com>
2014-03-15 21:54:19 +0100

    Major update of Test::WriteVariants based on original DBI-Test prototype

---
+++ lib/Test/WriteVariants.pm
@@ -0,0 +1,269 @@
+package Test::WriteVariants;
+
+use strict;
+use warnings;
+use autodie;
+
+use File::Find;
+use File::Path;
+use File::Basename;
+use Module::Pluggable::Object;
+use Carp qw(croak confess);
+
+use Test::WriteVariants::Context qw(new_value_list);
+use Data::Tumbler;
+
+our $VERSION = '0.001';
+
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $self = bless {} => $class;
+
+    for my $attribute (qw(allow_dir_overwrite allow_file_overwrite)) {
+        next unless exists $args{$attribute};
+        $self->$attribute(delete $args{$attribute});
+    }
+    confess "Unknown $class arguments: @{[ keys %args ]}"
+        if %args;
+
+    return $self;
+}
+
+
+# If the output directory already exists when tumble() is called it'll
+# throw an exception (and warn if it wasn't created during the run).
+# Setting allow_dir_overwrite true disables this safety check.
+sub allow_dir_overwrite {
+    my $self = shift;
+    $self->{allow_dir_overwrite} = shift if @_;
+    return $self->{allow_dir_overwrite};
+}
+
+# If the test file that's about to be written already exists
+# then write_output_files() will throw an exception.
+# Setting allow_file_overwrite true disables this safety check.
+sub allow_file_overwrite {
+    my $self = shift;
+    $self->{allow_file_overwrite} = shift if @_;
+    return $self->{allow_file_overwrite};
+}
+
+
+sub write_test_variants {
+    my ($self, %args) = @_;
+
+    my $input_tests = delete $args{input_tests}
+        or croak "input_tests not specified";
+    my $variant_providers = delete $args{variant_providers}
+        or croak "variant_providers not specified";
+    my $output_dir = delete $args{output_dir}
+        or croak "output_dir not specified";
+    croak "write_test_variants: unknown arguments: @{[ keys %args ]}"
+        if keys %args;
+
+    croak "write_test_variants: $output_dir already exists"
+        if -d $output_dir and not $self->allow_dir_overwrite;
+
+    my $tumbler = Data::Tumbler->new(
+        consumer => sub {
+            my ($path, $context, $payload) = @_;
+            # payload is a clone of input_tests possibly modified by providers
+            $self->write_output_files($path, $context, $payload, $output_dir);
+        },
+        add_context => sub {
+            my ($context, $item) = @_;
+            return $context->new($context, $item);
+        },
+    );
+
+    $tumbler->tumble(
+        $self->_normalize_providers($variant_providers),
+        [],
+        Test::WriteVariants::Context->new(),
+        $input_tests, # payload
+    );
+
+    warn "No tests written to $output_dir!\n"
+        if not -d $output_dir and not $self->allow_dir_overwrite;
+
+    return;
+}
+
+
+
+# ------
+
+# XXX also implement a find_input_test_files - that fines .t files
+
+sub find_input_test_modules {
+    my ($self, %args) = @_;
+
+    my $namespaces = delete $args{search_path}
+        or croak "search_path not specified";
+    my $input_tests = delete($args{input_tests}) || {};
+    croak "find_input_test_modules: unknown arguments: @{[ keys %args ]}"
+        if keys %args;
+
+    my $namespaces_regex = join "|", map { quotemeta($_) } @$namespaces;
+    my $namespaces_qr    = qr/^($namespaces_regex)::/;
+
+    my @test_case_modules = Module::Pluggable::Object->new(
+        require => 0,
+        search_path => $namespaces,
+    )->plugins;
+
+    for my $module_name (@test_case_modules) {
+        $self->add_test_module($input_tests, $module_name, $namespaces_qr);
+    }
+
+    return $input_tests;
+}
+
+
+sub add_test_module {
+    my ($self, $input_tests, $module_name, $namespaces_qr) = @_;
+
+    # map module name, without the namespace prefix, to a dir path
+    my $test_name = $module_name;
+    $test_name =~ s/$namespaces_qr//;
+    $test_name =~ s{[^\w:]+}{_}g;
+    $test_name =~ s{::}{/}g;
+
+    $self->add_test($input_tests, $test_name, {
+        module => $module_name,
+    });
+
+    return;
+}
+
+
+sub add_test {
+    my ($self, $input_tests, $test_name, $new_test) = @_;
+
+    confess "Can't add test $test_name because a test with that name exists"
+        if $input_tests->{ $test_name };
+
+    $input_tests->{ $test_name } = $new_test;
+    return;
+}
+
+
+sub _normalize_providers {
+    my ($self, $input_providers) = @_;
+    my @providers = @$input_providers;
+
+    # if a provider is a namespace name instead of a code ref
+    # then replace it with a code ref that uses Module::Pluggable
+    # to load and run the provider classes in that namespace
+
+    for my $provider (@providers) {
+        next if ref $provider eq 'CODE';
+
+        my @test_variant_modules = Module::Pluggable::Object->new(
+            require => 1,
+            on_require_error     => sub { croak "@_" },
+            on_instantiate_error => sub { croak "@_" },
+            search_path => [ $provider ],
+        )->plugins;
+        @test_variant_modules = sort @test_variant_modules;
+
+        warn sprintf "Variant providers in %s: %s\n", $provider, join(", ", map {
+            (my $n=$_) =~ s/^${provider}:://; $n
+        } @test_variant_modules);
+
+        $provider = sub {
+            my ($path, $context, $tests) = @_;
+
+            my %variants;
+            # loop over several methods as a basic way of letting plugins
+            # hook in either early or late if they need to
+            for my $method (qw(provider_initial provider provider_final)) {
+                for my $test_variant_module (@test_variant_modules) {
+                    next unless $test_variant_module->can($method);
+                    #warn "$test_variant_module $method...\n";
+                    $test_variant_module->$method($path, $context, $tests, \%variants);
+                    #warn "$test_variant_module $method: @{[ keys %variants ]}\n";
+                }
+            }
+
+            return %variants;
+        };
+    }
+
+    return \@providers;
+}
+
+
+sub write_output_files {
+    my ($self, $path, $context, $input_tests, $output_dir) = @_;
+
+    my $base_dir_path = join "/", $output_dir, @$path;
+
+    for my $testname (sort keys %$input_tests) {
+
+        # note that $testname can include a subdirectory path
+        $testname .= ".t" unless $testname =~ m/\.t$/;
+        my $full_path = "$base_dir_path/$testname";
+
+        warn "Writing $full_path\n";
+
+        my $testinfo = $input_tests->{$testname};
+        my $test_script = $self->get_test_file_body($context, $testinfo);
+
+        $self->write_file($full_path, $test_script);
+    }
+
+    return;
+}
+
+
+sub write_file {
+    my ($self, $filepath, $content) = @_;
+
+    croak "$filepath already exists!\n"
+        if -e $filepath and not $self->allow_file_overwrite;
+
+    my $full_dir_path = dirname($filepath);
+    mkpath($full_dir_path, 0)
+        unless -d $full_dir_path;
+
+    open my $fh, ">", $filepath;
+    print $fh $content;
+    close $fh;
+
+    return;
+}
+
+
+sub get_test_file_body {
+    my ($self, $context, $testinfo) = @_;
+
+    my @body;
+
+    my $prologue = $testinfo->{prologue}
+        || qq{#!perl\nuse lib "lib";\n}; # XXX remove use lib
+
+    push @body, $context->get_code;
+
+    push @body, "use lib '$testinfo->{lib}';\n"
+        if $testinfo->{lib};
+
+    push @body, "require '$testinfo->{require}';\n"
+        if $testinfo->{require};
+
+    if (my $module = $testinfo->{module}) {
+        push @body, "require $module;\n";
+        push @body, "$module->run_tests;\n";
+    }
+
+    push @body, "$testinfo->{code}\n"
+        if $testinfo->{code};
+
+    return join "", @body;
+}
+
+
+
+1;
---
+++ lib/Test/WriteVariants/Context.pm
@@ -0,0 +1,185 @@
+package Test::WriteVariants::Context;
+
+use strict;
+
+my $ContextClass = __PACKAGE__;
+
+# a Context is an ordered list of various kinds of named values (such as env vars, our vars)
+# possibly including other Context objects.
+#
+# Values can be looked up by name. The first match will be returned.
+
+sub new {
+    my $class = shift;
+    $class = ref $class if ref $class;
+    return bless [ @_ ], $class;
+}
+
+
+sub new_composite { shift->new(@_) } # see Test::WriteVariants::Context::BaseItem
+
+
+sub push_var { # add a var to an existing config
+    my ($self, $var) = @_;
+    push @$self, $var;
+    return;
+}
+
+
+sub _new_var    {
+    my ($self, $t, $n, $v, %e) = @_;
+    my $var = $t->new($n, $v, %e);
+    return $self->new( $var ); # wrap var item in a context list
+}
+sub new_env_var    { shift->_new_var($ContextClass.'::EnvVar', @_) }
+sub new_our_var    { shift->_new_var($ContextClass.'::OurVar', @_) }
+sub new_module_use { shift->_new_var($ContextClass.'::ModuleUse', @_) }
+sub new_meta_info  { shift->_new_var($ContextClass.'::MetaInfo', @_) }
+
+
+# XXX should ensure that a given type+name is only output once (the latest one)
+sub get_code  {
+    my $self = shift;
+    my @code;
+    for my $setting (reverse @$self) {
+        push @code, (ref $setting) ? $setting->get_code : $setting;
+    }
+    return join "", @code;
+}
+
+
+sub get_var { # search backwards through list of settings, stop at first match
+    my ($self, $name, $type) = @_;
+    for my $setting (reverse @$self) {
+        next unless $setting;
+        my @value = $setting->get_var($name, $type);
+        return $value[0] if @value;
+    }
+    return;
+}
+
+sub get_env_var    { my ($self, $name) = @_; return $self->get_var($name, $ContextClass.'::EnvVar') }
+sub get_our_var    { my ($self, $name) = @_; return $self->get_var($name, $ContextClass.'::OurVar') }
+sub get_module_use { my ($self, $name) = @_; return $self->get_var($name, $ContextClass.'::ModuleUse') }
+sub get_meta_info  { my ($self, $name) = @_; return $self->get_var($name, $ContextClass.'::MetaInfo') }
+
+
+
+{
+    package Test::WriteVariants::Context::BaseItem;
+    use strict;
+    require Data::Dumper;
+    require Carp;
+
+    # base class for an item (a name-value-type triple)
+
+    sub new {
+        my ($class, $name, $value) = @_;
+
+        my $self = bless {} => $class;
+        $self->name($name);
+        $self->value($value);
+
+        return $self;
+    }
+
+    sub name {
+        my $self = shift;
+        $self->{name} = shift if @_;
+        return $self->{name};
+    }
+
+    sub value {
+        my $self = shift;
+        $self->{value} = shift if @_;
+        return $self->{value};
+    }
+
+    sub get_code  {
+        return '';
+    }
+
+    sub get_var {
+        my ($self, $name, $type) = @_;
+        return if $type && !$self->isa($type);  # empty list
+        return if $name ne $self->name;         # empty list
+        return $self->value;                    # scalar
+    }
+
+    sub quote_values_as_perl {
+        my $self = shift;
+        my @perl_values = map {
+            my $val = Data::Dumper->new([$_])->Terse(1)->Purity(1)->Useqq(1)->Sortkeys(1)->Dump;
+            chomp $val;
+            $val;
+        } @_;
+        Carp::confess("quote_values_as_perl called with multiple items in scalar context (@perl_values)")
+            if @perl_values > 1 && !wantarray;
+        return $perl_values[0] unless wantarray;
+        return @perl_values;
+    }
+
+    # utility method to get a new composite when you only have a value object
+    sub new_composite { $ContextClass->new(@_) }
+
+} # ::BaseItem
+
+
+{
+    package Test::WriteVariants::Context::EnvVar;
+    use strict;
+    use parent -norequire, 'Test::WriteVariants::Context::BaseItem';
+
+    # subclass representing a named environment variable
+
+    sub get_code {
+        my $self = shift;
+        my $perl_value = $self->quote_values_as_perl($self->{value});
+        my @lines = (
+            sprintf('$ENV{%s} = %s;', $self->{name}, $perl_value),
+            sprintf('END { delete $ENV{%s} }', $self->{name}), # for VMS
+        );
+        return join "\n", @lines, '';
+    }
+}
+
+
+{
+    package Test::WriteVariants::Context::OurVar;
+    use strict;
+    use parent -norequire, 'Test::WriteVariants::Context::BaseItem';
+
+    # subclass representing a named 'our' variable
+
+    sub get_code {
+        my $self = shift;
+        my $perl_value = $self->quote_values_as_perl($self->{value});
+        return sprintf 'our $%s = %s;%s', $self->{name}, $perl_value, "\n";
+    }
+}
+
+
+{
+    package Test::WriteVariants::Context::ModuleUse;
+    use strict;
+    use parent -norequire, 'Test::WriteVariants::Context::BaseItem';
+
+    # subclass representing 'use $name (@$value)'
+
+    sub get_code {
+        my $self = shift;
+        my @imports = $self->quote_values_as_perl(@{$self->{value}});
+        return sprintf 'use %s (%s);%s', $self->{name}, join(", ", @imports), "\n";
+    }
+}
+
+{
+    package Test::WriteVariants::Context::MetaInfo;
+    use strict;
+    use parent -norequire, 'Test::WriteVariants::Context::BaseItem';
+
+    # subclass that doesn't generate any code
+    # It's just used to convey information between plugins
+}
+
+1;
